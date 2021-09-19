ae10388a181d2454f2c321363ff89a337086a98f
fish895623 <dan990429+github@gmail.com>
2021-09-19 15:55:18 +0900

    argparse 로 제목이랑 내용 파싱

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+import argparse
 import smtplib
-import yaml
 import sys
+from email.mime.text import MIMEText
+
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", type=str, help="Title")
+parser.add_argument("-f", type=str, required=True, help="string to send")
+args = parser.parse_args()
 
 with open("config.yml") as config:
+    msg = MIMEText(args.f)
+    msg["Subject"] = args.t
     config = yaml.load(config, Loader=yaml.FullLoader)
-    s = smtplib.SMTP(config['SMTP_SERVER'], config['SMTP_PORT'])
+    s = smtplib.SMTP(config["SMTP_SERVER"], config["SMTP_PORT"])
     s.starttls()
-    s.login(config["SENDER"], config['TOKEN'])
-    s.sendmail(config["SENDER"], config["RECEIVER"], sys.argv[1])
+    s.login(config["SENDER"], config["TOKEN"])
+    s.sendmail(config["SENDER"], config["RECEIVER"], msg.as_string())
     s.quit()
